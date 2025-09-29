5c0c517ef91873635c1cf1997086b37279b503ef
tambrulee <thamiltontestrote@gmail.com>
2025-09-29 20:49:22 +0100

    Created view

---
+++ better_bill_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BetterBillProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'better_bill_project'
--- better_billing/settings.py
+++ better_billing/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2wj$$5k#6mwc*i7e%ybgymfqsv!&fx2(h!1i=th40wysr$nlij'
+SECRET_KEY = 'django-insecure-wtz_aawo1+u*^%a26pb0^sh-u2sa#t9v4e5zrf+$3fa)3#nv58'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'better_bill_project',
 ]
 
 MIDDLEWARE = [
