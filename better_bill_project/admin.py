ca223f05cd93009128b37fb2d81eddfba7560331
tambrulee <thamiltontestrote@gmail.com>
2025-10-05 14:50:21 +0100

    Changes to admin.py

--- better_bill_project/admin.py
+++ better_bill_project/admin.py
@@ -3,17 +3,6 @@
 from django.contrib import admin
 from .models import Client, Personnel, Matter, TimeEntry
 
-from django.contrib.auth import get_user_model
-User = get_user_model()
-u = User.objects.get(username="YOUR_USERNAME")
-u.is_staff, u.is_superuser   # should both be True
-# if not:
-u.is_staff = True
-u.is_superuser = True
-u.save()
-exit()
-
-
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ("client_number", "name", "phone", "contact")
