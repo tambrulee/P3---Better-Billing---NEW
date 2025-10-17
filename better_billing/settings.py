bd63b826e7b11558d9240a5da279313617b266c6
tambrulee <thamiltontestrote@gmail.com>
2025-10-17 11:19:31 +0100

    Added favicon

--- better_billing/settings.py
+++ better_billing/settings.py
@@ -156,6 +156,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'assets'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
