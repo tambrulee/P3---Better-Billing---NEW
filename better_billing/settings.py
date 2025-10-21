4f05ff86ab11b0ace8c7d5e9e9dca92a9b59cd19
tambrulee <thamiltontestrote@gmail.com>
2025-10-21 22:51:45 +0100

    Hide key

--- better_billing/settings.py
+++ better_billing/settings.py
@@ -21,8 +21,14 @@ DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+# SECURITY WARNING: keep the secret key used in production secret
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+if not SECRET_KEY:
+    SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+    if not SECRET_KEY:
+        raise RuntimeError("SECRET_KEY is not set")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
